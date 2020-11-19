8504ff5d03b278855e5d6c06064159e7ba798755
s00 <Orosie230@gmail.com>
2020-11-20 00:15:33 +0900

    updated clock

--- cristmas_clock.js
+++ cristmas_clock.js
@@ -1,34 +1,31 @@
 const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1");
 
+function getTime() {
+  const xMas = new Date("2020-12-25 00:00:00");
+  const now = new Date();
+  const distance = xMas - now;
+  
 
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
 
-function getTime(){
-    const xMas = new Date("2020-12-25 00:00:00");
-    const now = new Date();
-    const distance = xMas - now;
-    console.log(distance);
-    
-    const days = Math.floor(distance/(1000*60*60*24));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //% = remainder operator/ 나누고 난 나머지를 return
 
-    console.log(days);
-    const hours =Math.floor(distance%(1000*60*60*60)/(1000*60*60));
-    console.log(hours);
-    const minutes =Math.floor((distance%(1000*60*60))/(1000*60));
-    const seconds= Math.floor((distance%(1000*60))/1000);
-    //% = remainder operator/ 나누고 난 나머지를 return
-    
-    clockTitle.innerText 
-    = `D-${days<10? `0${days}` :`${days}`} ${hours<10? `0${hours}` : `${hours}`} : ${minutes<10? `0${minutes}` : `${minutes}`} : ${seconds < 10 ? `0${seconds}`:`${seconds}`}` ;
-
+  clockTitle.innerText = `D-${days < 10 ? `0${days}` : days} ${
+    hours < 10 ? `0${hours}` : hours
+  } : ${minutes < 10 ? `0${minutes}` : minutes} : ${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
 }
 
-
-
-
-function init(){
-getTime();
-setInterval(getTime, 1000);
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
 }
 init();
